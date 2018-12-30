afd8d54ac87fef42383e8c950a579e588b4b89c9
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-12-30 16:30:58 +0530

    added header and footer by making files explicitly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HomeModule} from './home/home.module'
+import { HeaderComponent } from './shared';
+import { FooterComponent } from './shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
